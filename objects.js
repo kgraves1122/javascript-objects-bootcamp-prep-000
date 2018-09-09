12f3186556cd29a6c61cb3c457cf452b9709f736
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-09 23:21:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,14 @@
-var playlist 
+var playlist = {
+  chrisBrown: 'Loyal',
+  rihanna: 'Work'
+};
+
+function updatePlaylist (obj, key, val) {
+  obj.key = val;
+  return obj;
+}
+
+function removeFromPlaylist (obj, key) {
+  delete obj.key;
+  return obj;
+}
